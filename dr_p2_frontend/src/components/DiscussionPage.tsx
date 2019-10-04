e6aba136edc975074af2ce07e272a3b6d84c3726
Simone Pierazzini <makeroo@gmail.com>
2019-10-04 19:09:26 +0200

    cleanup

--- dr_p2_frontend/src/components/DiscussionPage.tsx
+++ dr_p2_frontend/src/components/DiscussionPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { match, RouteComponentProps } from 'react-router-dom'
+import { RouteComponentProps } from 'react-router-dom'
 
 import { AppState } from '../store/index'
 
