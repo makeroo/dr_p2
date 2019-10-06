42f93506bed6e28f68663edb82453c4ecf79b4b4
Simone Pierazzini <makeroo@gmail.com>
2019-10-06 11:36:56 +0200

    load translations on demand instead of bundling them

--- dr_p2_frontend/src/i18n.js
+++ dr_p2_frontend/src/i18n.js
@@ -1,39 +1,31 @@
-
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import XHR from "i18next-xhr-backend";
 
-i18n.use(LanguageDetector).init({
-  // we init with resources
-  resources: {
-    en: {
-      translations: {
-        welcome_page: "Introduction"
-      }
-    },
+const availableLanguages = ['en', 'it'];
 
-    it: {
-      translations: {
-        welcome_page: "Introduzione"
-      }
-    }
-  },
-  fallbackLng: "en",
-  debug: true,
+i18n
+    .use(XHR)
+    .use(LanguageDetector)
+    .init({
+        fallbackLng: "en",
+        whitelist: availableLanguages,
+        debug: true,
 
-  // have a common namespace used around the full app
-  ns: ["translations"],
-  defaultNS: "translations",
+        // have a common namespace used around the full app
+//        ns: ["translations"],
+//        defaultNS: "translations",
 
-  keySeparator: false, // we use content as keys
+        keySeparator: false, // we use content as keys
 
-  interpolation: {
-    escapeValue: false, // not needed for react!!
-    formatSeparator: ","
-  },
+        interpolation: {
+            escapeValue: false, // not needed for react!!
+            formatSeparator: ","
+        },
 
-  react: {
-    wait: true
-  }
-});
+        react: {
+            wait: true
+        }
+    });
 
 export default i18n;
