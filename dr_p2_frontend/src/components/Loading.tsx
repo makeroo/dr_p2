a36141c87bc03ff1142e8938b37436cc25b2d2b8
Simone Pierazzini <makeroo@gmail.com>
2019-10-10 08:17:48 +0200

    loading component without layout

--- dr_p2_frontend/src/components/Loading.tsx
+++ dr_p2_frontend/src/components/Loading.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+
 const Loading: React.FC<{}> = () => {
-    return <div>loading...</div>
+    return (
+    <Container>
+        <Typography>loading...</Typography>
+        <CircularProgress/>
+    </Container>
+    )
 }
 
 export default Loading
