ae1786401226d4821132d53e3f60e03c4b0387d9
Simone Pierazzini <makeroo@gmail.com>
2019-10-21 08:32:39 +0200

    solution browsing: pagination complete

--- dr_p2_frontend/src/components/discussion_page/Solutions.tsx
+++ dr_p2_frontend/src/components/discussion_page/Solutions.tsx
@@ -11,8 +11,10 @@ import Button from '@material-ui/core/Button'
 import Fab from '@material-ui/core/Fab'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
-import AddIcon from '@material-ui/icons/Add'
 import TextField from '@material-ui/core/TextField'
+import AddIcon from '@material-ui/icons/Add'
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
+import ChevronRight from '@material-ui/icons/ChevronRight'
 
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
@@ -23,7 +25,7 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import i18n from 'i18next'
 
 import { AppState } from '../../store/index'
-import { addDialog } from '../../store/explorer/actions'
+import { addDialog, solutionsSelectPage } from '../../store/explorer/actions'
 import { postThesis } from '../../store/discussion/actions';
 
 const mapStateToProps = (state: AppState) => ({
@@ -43,6 +45,9 @@ const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>) => ({
     },
     postThesis: (is_solution: boolean, content: string) => {
         return dispatch(postThesis(is_solution, content))
+    },
+    gotoPage: (page: number) => {
+        return dispatch(solutionsSelectPage(page))
     }
 })
 
@@ -59,18 +64,35 @@ const useStyles = makeStyles((theme: Theme) =>
             border: 0,
             outline: 'none',
         },
+        prevPage: {
+            width: '36px',
+            height: '36px',
+            position: 'absolute',
+            left: theme.spacing(0.5),
+            minWidth: '36px',
+        },
+        nextPage: {
+            width: '36px',
+            height: '36px',
+            position: 'absolute',
+            right: theme.spacing(0.5),
+            minWidth: '36px',
+        },
+        solution: {
+            minHeight: '4em',
+        },
     }),
 )
 
 const Solutions : React.FC<SolutionsProps> = (props) => {
     const classes = useStyles();
 
-    const { indexedDiscussion, openAddDialog, closeAddDialog, addOpen, postThesis } = props
+    const { indexedDiscussion, openAddDialog, closeAddDialog, addOpen, postThesis, gotoPage } = props
     var { page } = props
     const { solutions, theses, invertedSupports } = indexedDiscussion
 
     const theme = useTheme<Theme>()
-    let visibleSolutions
+    let visibleSolutions : number
 
     const inputEl = useRef<HTMLInputElement>(null)
 
@@ -85,18 +107,22 @@ const Solutions : React.FC<SolutionsProps> = (props) => {
         }
     }
 
-    const isSmall = useMediaQuery(theme.breakpoints.down('md'))
-    const isMedium = useMediaQuery(theme.breakpoints.down('lg'))
+    const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
+    const isMedium = useMediaQuery(theme.breakpoints.down('md'))
+    let pageStep : number
 
     if (isSmall) {
         visibleSolutions = 1
+        pageStep = 1
     } else if (isMedium) {
         visibleSolutions = 3;
+        pageStep = 1
     } else {
         visibleSolutions = 6;
+        pageStep = 2
     }
 
-    let pages = Math.ceil(solutions.length / visibleSolutions)
+    let pages = solutions.length // Math.ceil(solutions.length / visibleSolutions)
 
     if (pages < visibleSolutions) {
         page = 0;
@@ -106,7 +132,19 @@ const Solutions : React.FC<SolutionsProps> = (props) => {
         page = pages - visibleSolutions;
     }
 
-    const fromSol = page * visibleSolutions;
+    const prevPage = () => {
+        if (page > 0) {
+            gotoPage(Math.max(page - pageStep, 0))
+        }
+    }
+
+    const nextPage = () => {
+        if (page < solutions.length - visibleSolutions) {
+            gotoPage(Math.min(page + pageStep, solutions.length - visibleSolutions))
+        }
+    }
+
+    const fromSol = page;
     const toSol = Math.min(fromSol + visibleSolutions, solutions.length)
 
     let columns : JSX.Element[] = []
@@ -130,13 +168,25 @@ const Solutions : React.FC<SolutionsProps> = (props) => {
         }
 
         columns.push(
-            <Grid item container xs={12} md={4} lg={1} key={solution.id}>
+            <Grid item container xs={12} md={4} lg={2} key={solution.id}>
                 <Grid item xs={12}>
-                    <Paper>{solution.content}</Paper>
+                    <Paper className={classes.solution}>{solution.content}</Paper>
                 </Grid>
                 <Grid item xs={12} container>
                     { thesesElements }
                 </Grid>
+                { solutions.length > visibleSolutions &&
+                    <React.Fragment>
+                        <Button aria-label="previous" className={classes.prevPage}
+                            onClick={prevPage}>
+                            <ChevronLeft/>
+                        </Button>
+                        <Button color="default" aria-label="next" className={classes.nextPage}
+                            onClick={nextPage}>
+                            <ChevronRight/>
+                        </Button>
+                    </React.Fragment>
+                }
             </Grid>
         )
     }
