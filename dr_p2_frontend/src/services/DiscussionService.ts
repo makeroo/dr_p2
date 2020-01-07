5054489b9fb59e93c6865267b5199095178a5a94
Simone Pierazzini <makeroo@gmail.com>
2020-01-07 18:49:05 +0100

    prevent multiple queries to discussion service

--- dr_p2_frontend/src/services/DiscussionService.ts
+++ dr_p2_frontend/src/services/DiscussionService.ts
@@ -1,18 +1,19 @@
 import { Discussion, Voting, Thesis, Relation, RelationType } from "../store/discussion/types"
+import { singleRequest1 } from "../utils/single_request"
 
 class DiscussionService {
     private fake_ids = 10
 
-    newProblem(question: string): Promise<number> {
+    newProblem = singleRequest1((question: string): Promise<number> => {
         return new Promise<number>((resolve) => {
             setTimeout(() => {
                 resolve(1)
             }, 1000)
         })
 
-    }
+    })
 
-    getDiscussion(id: number): Promise<Discussion> {
+    getDiscussion = singleRequest1((id: number): Promise<Discussion> => {
         return new Promise<Discussion>((resolve) => {
             setTimeout(() => {
                 const discussion = {
@@ -117,9 +118,9 @@ class DiscussionService {
                 resolve(discussion)
             })
         })
-    }
+    })
 
-    getVoting(id: number): Promise<Voting> {
+    getVoting = singleRequest1((id: number): Promise<Voting> => {
         return new Promise<Voting>((resolve) => {
             setTimeout(() => {
                 const voting : Voting = {
@@ -130,36 +131,46 @@ class DiscussionService {
                 resolve(voting)
             }, 1000)
         })
+    })
+
+    postThesis (is_solution: boolean, content: string): Promise<Thesis> {
+        const thesis = {
+            id: this.fake_ids++,
+            solution: is_solution,
+            content
+        }
+
+        return this._postThesis(thesis)
     }
 
-    postThesis(is_solution: boolean, content: string): Promise<Thesis> {
+    private _postThesis = singleRequest1((thesis: Thesis): Promise<Thesis> => {
         return new Promise<Thesis>((resolve) => {
             setTimeout(() => {
-                const thesis = {
-                    id: this.fake_ids++,
-                    solution: is_solution,
-                    content
-                }
 
                 resolve(thesis)
             }, 1000)
         })
-    }
+    })
 
     postRelation(thesis1: Thesis, thesis2: Thesis, relationType: RelationType): Promise<Relation> {
+        const relation = {
+            id: this.fake_ids++,
+            thesis1: thesis1.id,
+            thesis2: thesis2.id,
+            type: relationType
+        }
+
+        return this._postRelation(relation)
+    }
+
+    private _postRelation = singleRequest1((relation: Relation): Promise<Relation> => {
         return new Promise<Relation>((resolve) => {
             setTimeout(() => {
-                const relation = {
-                    id: this.fake_ids++,
-                    thesis1: thesis1.id,
-                    thesis2: thesis2.id,
-                    type: relationType
-                }
 
                 resolve(relation)
             }, 1000)
         })
-    }
+    })
 }
 
 export default DiscussionService
